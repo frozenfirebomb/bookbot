b8f366bde6d0bea41ce67d0bfbc81df9245e42d0
frozenfirebomb <bartvrugt@hotmail.com>
2025-06-11 19:36:26 -0400

    main.py now prints the book to console.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def main ():
+        print(get_book_text("books/frankenstein.txt"))
+        pass
+
+def get_book_text(fp):
+	with open(fp) as f:
+		file_contents = f.read()
+	return file_contents
+
+main()

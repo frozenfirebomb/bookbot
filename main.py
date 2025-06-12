f5e90aa837bddbe6fb79da12c2a89b98e1befcb4
frozenfirebomb <bartvrugt@hotmail.com>
2025-06-11 20:30:25 -0400

    main.py now states the number of words in a file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 def main ():
-        print(get_book_text("books/frankenstein.txt"))
-        pass
+	book_path = "books/frankenstein.txt"
+	text = get_book_text(book_path)
+	print(word_count(text), "words found in the document")
+	pass
 
-def get_book_text(fp):
+def get_book_text(fp):        #this function returns the contents of a file as a string from a file path provided as a string
 	with open(fp) as f:
 		file_contents = f.read()
 	return file_contents
 
+def word_count(text):
+	return len(text.split())
 main()

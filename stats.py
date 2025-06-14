46f7c774da3c0ebcbe417629aba1692aa033994c
frozenfirebomb <bartvrugt@hotmail.com>
2025-06-14 12:04:14 -0400

    first attempt to create a list of dictionary pairs

--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
-def word_count(text):		# this counts the number of words a string given to text.
+def word_count(text):		# this counts the number of words a string given to the value text.
         return len(text.split())
 
-def character_count(text):	# trying to count individual characters after setting them all to lowercase
+def character_count(text):	# counts individual characters after setting them all to lowercase
 	characters = {}
 	text_lower = text.lower()
 	for char in text_lower:
@@ -10,3 +10,13 @@ def character_count(text):	# trying to count individual characters after setting
 		if char in characters:
 			characters[char] += 1
 	return characters
+
+def character_sort(character_count(text)):
+	list_of_character_dicts = []
+	character_dict = {}
+	for char in character_count:
+		character_dict["char"] = char
+		character_dict["num"] = char
+		list_of_character_dicts.append(character_dict)
+		pass
+
